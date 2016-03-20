3b0be28a2dd2dbfeaf2c5426964c9d2a4793e6db
kato_ryota <kato.ryota.0124@gmail.com>
2016-03-20 14:54:52 +0900

    ハードコードしてた部分を定数ファイルから参照するように変更

--- api_const.py
+++ api_const.py
@@ -22,15 +22,15 @@ import json
 # }}}
 
 class Result(object) :
-    SUCCESS_001 = {code : 'SUCCESS_001', message : u'正常終了'}
+    SUCCESS_001 = {'code' : 'SUCCESS_001', 'message' : u'正常終了'}
 
 # エラー
 class Error(object):
-    SUCCESS_001 = {code : 'SUCCESS_001', message : u'正常終了'}
-    ERROR_001 = {code : 'ERROR_001', message : u'アクセストークン不正'}
-    ERROR_002 = {code : 'ERROR_002', message : u'認証エラー'}
-    ERROR_003 = {code : 'ERROR_003', message : u'KVSへのデータ登録時に異常発生'}
-    WARN_001 = {code : 'WARN_001', message : u'警告'}
+    SUCCESS_001 = {'code' : 'SUCCESS_001', 'message' : u'正常終了'}
+    ERROR_001   = {'code' : 'ERROR_001',   'message' : u'アクセストークン不正'}
+    ERROR_002   = {'code' : 'ERROR_002',   'message' : u'認証エラー'}
+    ERROR_003   = {'code' : 'ERROR_003',   'message' : u'KVSへのデータ登録時に異常発生'}
+    WARN_001    = {'code' : 'WARN_001',    'message' : u'警告'}
 
 # 後処理 {{{
 # }}}
